9df0c9ae771a663f6f15a8d673c5952886ee78c1
tiagodias1912 <164020857+tiagodias1912@users.noreply.github.com>
2024-05-03 14:42:51 -0300

    Alteracao TipoVariaveis

---
+++ tipos-variaveis/src/TipoVariaveis.java
@@ -0,0 +1,22 @@
+public class TipoVariaveis {
+
+    public static void main(String arg[]){
+
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+        
+    }
+    
+}
