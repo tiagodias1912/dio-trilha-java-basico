4bcdfe6abecfe052e97b0c0152830e9408c8bffd
tiagodias1912 <164020857+tiagodias1912@users.noreply.github.com>
2024-05-06 14:27:00 -0300

    Nova entrada operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,19 @@
+public class Operadores {
+    public static void main (String arg[]){
+
+            boolean condicao1=true;
+            boolean condicao2=false;
+
+            if (condicao1 && condicao2) {
+                System.out.println("uma das condioes sao verdadeira");
+                
+            }
+
+       
+    
+    
+
+
+    }
+    
+}
