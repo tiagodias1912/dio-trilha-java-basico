2dc7fac3de6ce357fcc2db3fc1948918c1e04569
Tiago <164020857+tiagodias1912@users.noreply.github.com>
2024-07-12 14:15:44 -0300

    Add files via upload

---
+++ Desafio-Projeto-DIO/src/SingletonLazyHolder.java
@@ -0,0 +1,14 @@
+public class SingletonLazyHolder {
+
+    private static class InstaceHolder{
+        public static SingletonLazyHolder instancia = new SingletonLazyHolder();
+    }
+
+        private SingletonLazyHolder(){
+        super();
+        }
+
+        public static SingletonLazyHolder getInstancia(){
+            return InstaceHolder.instancia;
+    }
+}
